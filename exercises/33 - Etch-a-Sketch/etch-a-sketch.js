02d8e54e8ba305e312d9752ead73ad14dc451841
Victor Chiang <vgchiang@gmail.com>
2020-07-23 21:45:34 -0500

    completed etch a sketch challenge

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -8,15 +8,83 @@ const ctx = canvas.getContext('2d');
 
 const shakeButton = document.querySelector('.shake');
 
+const MOVE_AMOUNT = 50;
+
 // Setup our canvas for drawing
+
+// destructuring.  variables created from properties named exactly the same thing.
+
+// const width = canvas.width;
+// const height = canvas.height;
+
+const {width, height} = canvas
+
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
+console.log(width, height);
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
 ctx.beginPath(); // start the drawing
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
 
+function draw( {key}) {
+  // increment the hue
+  hue += 10;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  console.log(key);
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT
+      break;
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+function handleKey(e) {
+  console.log(e);
+  if (e.key.includes('Arrow')) {
+  e.preventDefault();
+  draw({ key: e.key })
+  }
+}
+
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener('animationend',
+    function() {
+      canvas.classList.remove('shake');
+      console.log('done the shake!');
+  }, { once: true }
+  );
+}
+
+window.addEventListener('keydown', handleKey);
+
+shakeButton.addEventListener('click', clearCanvas);
 
