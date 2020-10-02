66d2ba1463c2d3cf4b9b39cb2b225995e7bb9dfa
Victor Chiang <vgchiang@gmail.com>
2020-10-01 21:17:57 -0500

    start of shopping list exericse

---
+++ exercises/57 - Shopping List/shopping.js
@@ -0,0 +1,39 @@
+const shoppingForm = document.querySelector(".shopping");
+const list = document.querySelector(".list");
+
+// we need an array to hold our state
+const items = [];
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log("submitted!");
+  // console.log(e.currentTarget);
+  const name = e.currentTarget.item.value;
+  // console.log(name);
+  const item = {
+    name: name,
+    id: Date.now(),
+    complete: false,
+  };
+  // Push the items into our state
+  items.push(item);
+  console.log(`There are now ${items.length} in your state`);
+  // clear the form
+  // e.currentTarget.item.value = '';
+  e.target.reset();
+  // fire off a custom event that will tell anyone else who cares that the items have been updated.
+  // displayItems();
+}
+
+function displayItems() {
+  const html = items.map(
+    item => `<li class="shopping-item">
+    <input type="checkbox">
+    <span class="itemName">${item.name}</span>
+    <button aria-label="Remove ${item.name}">&times;</button>
+    </li>`).join('');
+  // console.log(html);
+  list.innerHTML = html;
+}
+
+shoppingForm.addEventListener('submit', handleSubmit);
